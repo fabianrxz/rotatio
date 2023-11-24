32d6861aa7ef755b57eb7b03b9b7dccc94ddebcf
Fabian Ramirez <fabianramirez@pm.me>
2023-11-24 05:29:46 -0300

    agregando codigo para leer el archivo

--- codigo.asm
+++ codigo.asm
@@ -1,78 +1,45 @@
 section .data
     filename db 'radio.txt', 0
-    buffer db 10          ; Buffer para almacenar el número leído
-    num     dd 0          ; Variable para almacenar el número leído
-
-section .bss
-    fd      resd 1        ; Descriptor de archivo
+    buffer db 10   ; buffer
+    file_descriptor dd 0
+    number db 10   ; array para almacenar el número leído
 
 section .text
     global _start
 
 _start:
-    ; Abrir el archivo
-    mov eax, 5           ; syscall para open
-    mov ebx, filename    ; puntero al nombre del archivo
-    mov ecx, 0           ; modo de apertura (O_RDONLY)
-    int 0x80             ; realizar la llamada al sistema
+    ; Abre el archivo
+    mov eax, 5               ; Número de la llamada al sistema para abrir un archivo
+    mov ebx, filename        ; Puntero al nombre del archivo
+    mov ecx, 0               ; Modo de apertura (0 para lectura)
+    int 0x80                 ; Interrupción para llamar al sistema
 
-    ; Comprobar errores al abrir el archivo
-    cmp eax, -1
-    je  error_exit
+    ; Guarda el descriptor de archivo devuelto
+    mov [file_descriptor], eax
 
-    ; Asignar el descriptor de archivo a fd
-    mov [fd], eax
+    ; Lee el número del archivo
+    mov eax, 3               ; Número de la llamada al sistema para leer desde un archivo
+    mov ebx, [file_descriptor]
+    mov ecx, number          ; Puntero al array para almacenar el número
+    mov edx, 10              ; Tamaño del buffer
+    int 0x80                 ; Interrupción para llamar al sistema
 
-    ; Leer el número del archivo
-    mov eax, 3           ; syscall para read
-    mov ebx, [fd]        ; descriptor de archivo
-    mov ecx, buffer      ; puntero al búfer
-    mov edx, 10          ; número de bytes a leer
-    int 0x80             ; realizar la llamada al sistema
+    ; Realiza cálculos con el número leído (aquí puedes agregar tus operaciones)
+    ; ...
 
-    ; Convertir la cadena a un número
-    mov eax, buffer      ; puntero al búfer
-    call atoi            ; función para convertir la cadena a un número
-    mov [num], eax       ; almacenar el número en la variable
+    ; Muestra el resultado por consola
+    mov eax, 4               ; Número de la llamada al sistema para escribir en la consola
+    mov ebx, 1               ; Descriptor de archivo estándar para salida (1 para stdout)
+    mov ecx, number          ; Puntero a la matriz que contiene el número leído
+    mov edx, 10              ; Tamaño del buffer
+    int 0x80                 ; Interrupción para llamar al sistema
 
-    ; Realizar operaciones con el número (agrega tu lógica aquí)
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, num
-    mov edx, 8
-    int 0x80
-
-    ; Cerrar el archivo
-    mov eax, 6           ; syscall para close
-    mov ebx, [fd]        ; descriptor de archivo
-    int 0x80             ; realizar la llamada al sistema
+    ; Cierra el archivo
+    mov eax, 6               ; Número de la llamada al sistema para cerrar un archivo
+    mov ebx, [file_descriptor]
+    int 0x80                 ; Interrupción para llamar al sistema
 
     ; Salir del programa
-    mov eax, 1           ; syscall para exit
-    xor ebx, ebx         ; código de salida 0
-    int 0x80             ; realizar la llamada al sistema
-
-error_exit:
-    ; Manejar error al abrir el archivo (agrega tu lógica aquí)
-
-; Función para convertir una cadena a un número entero
-; Entrada: eax = puntero al inicio de la cadena
-; Salida: eax = número entero
-atoi:
-    xor ebx, ebx         ; Inicializar el resultado en ebx
-    xor ecx, ecx         ; Inicializar el índice en ecx
-
-convert_loop:
-    movzx edx, byte [eax + ecx] ; Cargar el siguiente carácter de la cadena en edx
-    cmp edx, 0           ; ¿Fin de la cadena?
-    je  convert_done
-
-    sub edx, '0'         ; Convertir de carácter a dígito
-    imul ebx, ebx, 10    ; Multiplicar el resultado actual por 10
-    add ebx, edx         ; Sumar el dígito al resultado
-    inc ecx              ; Mover al siguiente carácter
-    jmp convert_loop
-
-convert_done:
-    mov eax, ebx         ; El resultado está en ebx
-    ret
+    mov eax, 1               ; Número de la llamada al sistema para salir
+    xor ebx, ebx             ; Código de salida 0
+    int 0x80                 ; Interrupción para llamar al sistema
